a8455f02f17035a62e53596319b7b3f2bf75ba2f
Robert Isoski <r@robiso.com>
2021-04-11 18:09:35 +0200

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,56 +1,55 @@
 function Blog() {
     this.active = window.location.pathname.indexOf("blog") > -1;
 
-    this.init = function() {
+    this.init = function () {
         /* Check for summernote */
         var scripts = [].slice.call(document.getElementsByTagName("script"));
-        if(scripts.filter(e => e.src.indexOf("summernote") >= 0).length > 0 && blog.active) {
+        if (scripts.filter(e => e.src.indexOf("summernote") >= 0).length > 0 && blog.active) {
             // summernote.focus
-            $('.editable').on('summernote.blur', function(element) {
-              $("#save").show();
-              var value = $(element.currentTarget).summernote('code'),
-                  key = element.currentTarget.id;
+            $('.editable').on('summernote.blur', function (element) {
+                $("#save").show();
+                var value = $(element.currentTarget).summernote('code'),
+                    key = element.currentTarget.id;
 
-              blog.store(key, value, false);
+                blog.store(key, value, false);
             });
         }
     }
 
-    this.save = function(elem, reload) {
+    this.save = function (elem, reload) {
         // Let's save this element.
         $("#save").show();
 
-        var key = elem.id,
-            value = elem.value;
+        var key = elem.id.replace('_field',''),
+            value = elem.value.replace('_field','');
 
         blog.store(key, value, reload);
-
     };
 
-    this.store = function(key, value, reload) {
+    this.store = function (key, value, reload) {
         var page = decodeURI(window.location.pathname.split("/").pop());
 
-        $.post( rootURL + "/plugins/simple-blog/save.php", { key: key, value: value, page: page, token: token })
-            .done(function( data ) {
-                if(data) alert(data);
+        $.post(rootURL + "/plugins/simple-blog/save.php", {key: key, value: value, page: page, token: token})
+            .done(function (data) {
+                if (data) alert(data);
                 $("#save").hide();
 
                 // I really don't like this, but it's default behaviour on cms pages so will honor this.
-                if(reload) history.go(0);
+                if (reload) history.go(0);
             });
     }
 
-    this.new = function() {
+    this.new = function () {
 
         // Ask for name
         var name = prompt("New post title");
-        if(name && name.trim() != "") {
+        if (name && name.trim() != "") {
 
             // Let's save this element.
             $("#save").show();
 
-            $.post( rootURL + "/plugins/simple-blog/new.php", { page: name, token: token })
-                .done(function( data ) {
+            $.post(rootURL + "/plugins/simple-blog/new.php", {page: name, token: token})
+                .done(function (data) {
                     window.location.href = data;
                     $("#save").hide();
                 });
@@ -66,7 +65,14 @@ window.addEventListener('load', blog.init);
 
 // Overwrite default save method for editors other than summernote:
 
-window.fieldSave = (a, b, c) => {
-    $("#save").show();
-    blog.store(a, b, true);
-};
+window.addEventListener('click', function (e) {
+    const editableText = document.querySelectorAll('div.editText.editTextOpen:not(:hover) textarea');
+
+    editableText.forEach((editableElement) => {
+        e.stopPropagation();
+        e.preventDefault();
+
+        $("#save").show();
+        blog.save(editableElement, true);
+    });
+});
